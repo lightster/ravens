76becfa1fbdcfb2348634336d7045d5ae682b42b
Matt Light <matt.light@lightdatasys.com>
2017-08-19 01:47:59 -0700

    Allow Makefile to be used in docker or shell
    The Makefile now is generated by running `./configure {docker|shell}`,
    and the proper command prefix is used in the Makefile based on the
    environment requested.

--- docker/fs/usr/local/bin/entrypoint.sh
+++ docker/fs/usr/local/bin/entrypoint.sh
@@ -2,17 +2,16 @@
 
 case "$1" in
     test)
-        php /ravens/vendor/bin/phpunit
+        php vendor/bin/phpunit
         ;;
 
     install)
-        cd /ravens
         composer install
         php -r '
-            $config = require_once "/ravens/config/dist/config.test.php";
+            $config = require_once "config/dist/config.test.php";
             $config['test']['rabbitmq']['host'] = 'rabbitmq';
             $config_text = "<?php\nreturn " . var_export($config, true) . ";";
-            file_put_contents("/ravens/config/config.test.php", $config_text);
+            file_put_contents("config/config.test.php", $config_text);
         '
         ;;
 
